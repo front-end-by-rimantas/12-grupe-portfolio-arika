cf1c270827f98796b8ed50507c64977d62f008d2
indre.gatulyte <indre.gatulyte@gmail.com>
2020-01-08 15:25:50 +0200

    frame

--- js/actions.js
+++ js/actions.js
@@ -1,2 +1,11 @@
 "use strict";
 
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    document.getElementById("headeriux").className = "header";
+  } else {
+    document.getElementById("headeriux").className = "";
+  }
+}
