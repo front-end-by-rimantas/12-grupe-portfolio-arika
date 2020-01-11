15f256f7af5d6533eeb5a87260f8c96d5134654c
Ramune <rlisauskaite@gmail.com>
2020-01-11 23:28:41 +0200

    Start of the service sector

--- js/data.js
+++ js/data.js
@@ -6,7 +6,38 @@
 
 
 /* Ramunes script start*/
-
+const service = [
+    {
+        icon: 'archive',
+        title: 'Ramune',
+        text: Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil, vel
+    },
+    {
+        icon: 'archive',
+        title: 'Ramune',
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil, vel?'
+    },
+    {
+        icon: 'archive',
+        title: 'Ramune',
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil, vel?'
+    },
+    {
+        icon: 'archive',
+        title: 'Ramune',
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil, vel?'
+    },
+    {
+        icon: 'archive',
+        title: 'Ramune',
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil, vel?'
+    },
+    {
+        icon: 'archive',
+        title: 'Ramune',
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil, vel?'
+    },
+]
 /* Ramunes script end*/
 
 
--- js/functions.js
+++ js/functions.js
@@ -7,7 +7,20 @@
 
 
 /* Ramunes script start*/
+function renderService( data ) {
+    let HTML = '';
 
+    console.log(data);
+
+    HTML += `<div class="col-4 block">
+            <i class="fa fa-globe"></i>
+            <h2>frfedf</h2>   
+            <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil, vel?</h3>    
+          </div>`;
+    document.querySelector('#services > .row').innerHTML = HTML;
+    return;       
+
+}
 /* Ramunes script end*/
 
 
